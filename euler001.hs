f45ee67ce66cfe305be42a0fe80b718ec62a455a
Sefa Kilic <sefakilic@gmail.com>
2013-12-30 13:06:22 -0500

    practicing haskell

--- euler001.hs
+++ euler001.hs
@@ -1,1 +1,2 @@
+-- http://projecteuler.net/problem=1
 euler001 = sum [x | x <- [1..999], x `rem` 3 == 0 || x `rem` 5 == 0]
---
+++ euler005.hs
@@ -0,0 +1,2 @@
+-- http://projecteuler.net/problem=5
+euler005 = foldl lcm 1 [1..20]
